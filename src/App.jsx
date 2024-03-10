d851d9bb9d816faa5f0f6a5404a4fca2de486985
Ilija <slamkovilija4@gmail.com>
2024-03-10 21:26:21 +0100

    Css, Mobile Responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
+import React from 'react'
 import './App.css'
+import diceLogo from '../src/assets/images/icon-dice.svg'
+import desktopDivider from '../src/assets/images/pattern-divider-desktop.svg'
+import mobileDivider from '../src/assets/images/pattern-divider-mobile.svg'
 
 function App() {
 
   return (
     <>
-      <h1>Hello</h1>
+      <main>
+        <section>
+          <div className="container | padding-inline-400 padding-top-400">
+            <p className='text-primary-500 fw-extra-bold padding-top-400 fs-secondary'>ADVICE  <span>#117</span> </p>
+            <h1 className='fw-extra-bold text-primary-400 fs-primary padding-block-700'>
+              "It is easy to sit up and take notice, what's difficult is getting up and taking action"
+            </h1>
+            <div className='img | padding-top-500'></div>
+            <button className='btn'>
+              <img src={diceLogo} alt="diceLogo" />
+            </button>
+          </div>
+        </section>
+      </main>
     </>
   )
 }
